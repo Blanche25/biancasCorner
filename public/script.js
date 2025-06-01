1e248fff9b603f98f01e9a53f9af15e6aaf3d993
Blanche <biancadevietro@gmail.com>
2025-06-01 15:20:18 +0200

    new blog post, added fancy images, fixed template

--- public/script.js
+++ public/script.js
@@ -14,8 +14,39 @@ const posts = [
     date: "2025-02-03",
     file: "breaking nintendo's tos because i can.html",
   },
+  {
+    title: "linux has consumed me",
+    date: "2025-04-31",
+    file: "linux has consumed me.html",
+  },
 ];
+// Get all posts
+async function getAllPosts() {
+  const allPosts = posts
+    .filter((post) => post.file.endsWith(".html"))
+    .sort((a, b) => new Date(b.date) - new Date(a.date)); // Most recent first
 
+  return allPosts;
+}
+async function displayAllPosts() {
+  const outputElement = document.getElementById("allposts-list");
+  const allPosts = await getAllPosts();
+
+  const ul = document.createElement("ul");
+  allPosts.forEach((post) => {
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    a.href = `posts/${post.file}`;
+    a.textContent = `${post.date} - ${post.title}`;
+    li.appendChild(a);
+    ul.appendChild(li);
+  });
+
+  outputElement.appendChild(ul);
+}
+
+
+// Get 3 most recent posts
 async function getRecentPosts() {
   const recentPosts = posts
     .filter((post) => post.file.endsWith(".html"))
@@ -24,7 +55,6 @@ async function getRecentPosts() {
 
   return recentPosts;
 }
-
 async function displayPosts() {
   const outputElement = document.getElementById("posts-list");
   const recentPosts = await getRecentPosts();
@@ -42,4 +72,24 @@ async function displayPosts() {
   outputElement.appendChild(ul);
 }
 
-document.addEventListener("DOMContentLoaded", displayPosts);
+
+// Image stuff
+document.addEventListener('DOMContentLoaded', () => {
+  const overlay = document.getElementById('overlay');
+  const overlayImage = document.getElementById('overlayImage');
+  document.querySelectorAll('.image-item img').forEach(img => {
+    img.addEventListener('click', () => {
+      overlayImage.src = img.src;
+      overlay.style.display = 'flex';
+    });
+  });
+  overlay.addEventListener('click', () => {
+    overlay.style.display = 'none';
+    overlayImage.src = '';
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  displayPosts();
+  displayAllPosts();
+});
